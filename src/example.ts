b9efe94079d9aebda02e50fa9821a909b59a1908
The-BigMan-tech <saddlebox9@gmail.com>
2026-04-26 16:35:55 +0100

    added an example file

---
+++ src/example.ts
@@ -0,0 +1,10 @@
+import { SvalPlus } from "./index.js";
+
+
+class LangEvent {
+
+}
+export function langPoint(fn:(...args:any[])=>any,listener:(event:LangEvent)=>void) {
+    const interpreter = new SvalPlus();
+
+}
