61f2a22fc5a0a5e26804e65e8fdb28e3cef1862e
The-BigMan-tech <saddlebox9@gmail.com>
2026-04-26 17:24:16 +0100

    added the langpoint function structure

--- src/example.ts
+++ src/example.ts
@@ -1,10 +1,49 @@
-import { SvalPlus } from "./index.js";
+import { LangListener, SvalPlus } from "./index.ts";
 
+type Fn = (...args:any[])=>any;
 
-class LangEvent {
+export function langPoint<T extends Fn>(fn:T,listener:LangListener):T {
+    const interpreter = new SvalPlus({
+        ecmaVer:"latest", // Match your tsconfig target
+        sandBox: true, // Standard for eDSLs/Sandboxes
+    });
 
+    const fnString = fn.toString();
+    let fnName = fn.name 
+    let fnAssignment:string = '';
+
+    if (fnName.length === 0) {
+        const anonymous = 'anonymousFn';
+        fnAssignment = `var ${anonymous} = ${fnString}`
+        fnName = anonymous;
+    }else if (!fnName.startsWith('function')) {
+        fnAssignment = `var ${fnName} = ${fnString}`
+    }else {
+        fnAssignment = fnString;
+    }
+
+    const code = `
+        ${fnAssignment};
+        exports.result = ${fnName}(...args);
+    `
+    const newFn = (...args: any[]) => {
+        interpreter.import({ args });
+        interpreter.run(code);
+        return interpreter.exports.result;
+    };
+
+    interpreter.langListener = listener;
+    return newFn as T;
 }
-export function langPoint(fn:(...args:any[])=>any,listener:(event:LangEvent)=>void) {
-    const interpreter = new SvalPlus();
 
+const internalAdd = (a:number,b:number)=> {
+    return a + b
 }
+const add = langPoint(internalAdd,()=>{
+
+})
+const result = add(1,2);
+console.log(result);
+
+const result2 = add(2,3);
+console.log(result2);
