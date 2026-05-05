ca5bb64a41de34b8c0cfed1015b7c79399834989
The-BigMan-tech <saddlebox9@gmail.com>
2026-05-05 18:12:51 +0100

    updated the examples

--- src/example.ts
+++ src/example.ts
@@ -2,7 +2,7 @@ import { monitor } from "./index.ts";
 import chalk from "chalk";
 import { GenExe, LAZY_NODE } from "./monitored-events.ts";
 
-//the perf profiles dont include the parsing step.but thanks to meriyah,this is decently fast and its cached 
+//the perf profiles include the parsing and preprocessing step the monitor uses to build the code before it even executes it.Thanks to its caching,this only happens once and every call to that function takes significantly less time cuz it skips that step.
 
 function perf(fn:(...args:any[])=>void) {
     const start = performance.now();
@@ -35,29 +35,29 @@ perf(()=>{
 let count = 0;
 let otherNodes = 0;
 
-const add = monitor.fn({
-    main:{
-        ref:internalAdd, 
-    },
-    listener:(visit) => {
-        visit.is('AssignmentExpression',event => {
-            event.node.operator = "-=";//silently change the operator
-            count += 1;
-            console.log('Depth: ',event.scope.depth)
-            console.log('assignment result',visit.execute());
-        })
-        if (!visit.matched()) {
-            visit.is('Any',event=>{
-                otherNodes += 1;
-            })
-        }
-    },
-    beforeEachCall:()=>{
-        console.log('Entered the monitored add function');
-    }
-});
 
 perf(() => {
+    const add = monitor.fn({
+        main:{
+            ref:internalAdd, 
+        },
+        listener:(visit) => {
+            visit.is('AssignmentExpression',event => {
+                event.node.operator = "-=";//silently change the operator
+                count += 1;
+                console.log('Depth: ',event.scope.depth)
+                console.log('assignment result',visit.execute());
+            })
+            if (!visit.matched()) {
+                visit.is('Any',event=>{
+                    otherNodes += 1;
+                })
+            }
+        },
+        beforeEachCall:()=>{
+            console.log('Entered the monitored add function');
+        }
+    });
     const result = add(arrToAdd, hello);//passing an external dependency through its arguments
     console.log('Final Result:', result, 'Interceptions:', count,'Other nodes',otherNodes);
 });
@@ -72,15 +72,16 @@ const internalAdd2 = (a:number,b:number):number =>{
     hello2();
     return a + b;
 }
-const addClosure = monitor.fn({
-    main:{
-        ref:internalAdd2,
-        captures:{ hello2 },
-    },
-    listener:()=>undefined,
-});
+
 
 perf(() => {
+    const addClosure = monitor.fn({
+        main:{
+            ref:internalAdd2,
+            captures:{ hello2 },
+        },
+        listener:()=>undefined,
+    });
     const result = addClosure(1,3)
     console.log(result);
 });
@@ -90,6 +91,9 @@ async function asyncHello() {
     console.log('hello world');
     return 'Was Called'
 }
+
+const start = performance.now();
+
 const addPseudoClosure = monitor.fn({
     main:{
         ref:async(a: number, b: number)=>{
@@ -126,7 +130,6 @@ const addPseudoClosure = monitor.fn({
         console.log(`Seen the numbers a:${a} and b:${b}`);
     }
 });
-const start = performance.now();
 
 const result = await addPseudoClosure(4,8);
 console.log(result);
