f28d9a25a1fa9e9d8ee0cd249c5b99f0faa379d1
The-BigMan-tech <saddlebox9@gmail.com>
2026-07-20 21:47:13 +0100

    moved the examples folder

--- examples/example.ts
+++ examples/example.ts
@@ -124,12 +124,12 @@ const monitoredAsyncAdd = monitor({
     },
     inspector:function* (visit):InspectorGenerator {
         visit.is('CallExpression',event=>{
-            const calleeIndex = visit.localExeStack().length;
+            const stackLenAtCallee = visit.localExeStack().length;
             const callees = new Set()
 
             visit.perExecution = ()=>{
                 const stack = visit.localExeStack();//we dont consume the whole thing into an array to save performance
-                const element = stack.get(-(calleeIndex + 1));
+                const element = stack.get(-(stackLenAtCallee + 1));
                 // console.log('RESULT:',stack.get(0).evaluation);
                 if (!callees.has(element)) {
                     console.log('Callee:',element);
---
+++ examples/showcase.ts
@@ -0,0 +1,195 @@
+import { InspectorGenerator, monitor } from "../index.ts";
+
+
+//SHOWCASE 1
+//This shows how to get started and a general use case
+const zero = 0;
+
+const sumUp = (nums:number[])=> {
+    let sum:number = zero;
+    for (const num of nums) {
+        sum += num
+    }
+    return sum;
+}
+const monitoredSumUp = monitor({
+    main:{
+        ref:sumUp,
+        captures:{
+            zero//since zero is used by sumUp and is outside its scope,we capture it into the interpreter's context
+        } 
+    },
+    beforeEachCall:(nums)=>{
+        console.log('Entered the monitored sum up function with the nums: ',nums);
+    },
+    inspector:(visit) => {
+        visit.is('AssignmentExpression',event => {
+            event.node.operator = "-=";//silently change the operator
+            console.log('assignment result',visit.execute());
+        })
+        visit.is('ReturnStatement',event=>{
+            const result = visit.execute();
+            result.RES = 'I CHANGED THE VALUE';
+        })
+    },
+    afterEachCall:(result)=>{
+        console.log('result of the monitored function: ',result);
+    }
+});
+
+const arrToSum = [1,2,3,4,5,6,7,8,9,10];
+
+const result1 = sumUp(arrToSum)
+console.log('Result 1',result1);
+
+const result2 = monitoredSumUp(arrToSum)//the exact same call signature
+console.log('Result 2',result2);
+
+
+//SHOWCASE 2
+//This example will focus on embedding and not integrating the inspector hook
+const Printed = 'Printed: ';
+
+function print(str:string) {
+    console.log(Printed,str);
+}
+
+function sayHello() {
+    print('Hello world')
+}
+
+const generatedCode = {value:''}
+
+const monitoredSayHello = monitor({
+    main:{
+        ref:sayHello
+    },
+    embed:{
+        print:{//the object that maps the function name to the reference
+            ref:print,
+            captures:{//it can also state its own captures.and if we want,we could embed more functions and have the embedded print function call that.But lets keep things simple
+                Printed
+            }
+        }
+    },
+    sourceOut:generatedCode
+})
+
+monitoredSayHello();
+console.log('Generated code: \n',generatedCode.value);
+
+//Testing async and using the exe stack to get all the called functions during the function execution
+const monitoredAsyncSqrt = monitor({
+    main:{
+        ref:async (a: number)=>{
+            const sqrt = Math.sqrt(a);
+            const rounded = Number(sqrt.toFixed(3))
+            return await Promise.resolve(rounded);
+        }
+    },
+    inspector:function* (visit):InspectorGenerator {
+        visit.is('CallExpression',()=>{
+            const stackLenAtCallee = visit.localExeStack().length;
+            const callees = new Set()
+
+            visit.perExecution = ()=>{
+                const stack = visit.localExeStack();//we dont consume the whole thing into an array to save performance
+                const element = stack.get(-(stackLenAtCallee + 1));//in the stack,the latest values stay at the front and the oldest stay at the back.The callee node will stay at the back as each execution inserts a new result to the stack
+
+                if (!callees.has(element)) {
+                    console.log('Callee:',element);
+                    callees.add(element);
+                    return
+                }
+            }
+        });
+        visit.is('ReturnStatement',()=>{
+            visit.perExecution = ()=>{
+                const stack = visit.localExeStack()
+                console.log('seen awaited result: ',stack.get(0).evaluation);
+            }
+        })
+        yield visit.execute();//for async functions,we want to yield the execution to pause the inspector till it fully executes.but since we cant yield in the 'is' method,we do it outside.We must set our perExe hook before calling visit.execute for the hook to fire.which is why this is at the bottom
+    },
+});
+
+console.log('Monitored async sqrt: ',await monitoredAsyncSqrt(2)); 
+
+
+//Using the on step hook
+
+function perf(fn:(...args:any[])=>void) {
+    const start = performance.now();
+    fn();
+    const end = performance.now();
+    console.log(`\nFinished in ${end-start} milliseconds\n`);
+};
+
+function calculateAverage(numbers: number[],caller:'monitor' | 'js'): number {
+    if (caller === "monitor") {
+        while (true) {}//simulate an infinite loop
+    }
+    if (!numbers || numbers.length === 0) {
+        return 0;
+    }
+    
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return Number((sum / numbers.length).toFixed(3));
+}
+perf(()=>{
+    const result = calculateAverage([20,30,70,88,91,72],'js');
+    console.log('\nThe average is: ',result);
+});
+
+
+type milliseconds = number;
+
+function timeFn<T extends (...args:any[])=>void>(fn:T,budget:milliseconds):T {
+    const fnBuildStart = performance.now();
+
+    const graceTime = 0.5 as milliseconds;
+    let startTime = 0 as milliseconds;
+    let usedTime = 0 as milliseconds;
+    let step = 0;
+
+    const checkBudget = ()=>{
+        usedTime = (performance.now() - startTime);
+        if (usedTime > (budget + graceTime)) {
+            throw new Error(`A work function used ${usedTime.toFixed(3)}ms when only given a budget of ${budget.toFixed(3)}ms.`);
+        };
+    };
+    const monitoredFn = monitor({
+        main:{
+            ref:fn,
+        },
+        beforeEachCall: () => {
+            startTime = performance.now()
+            usedTime = 0;
+            step = 0;
+        },
+        onStep:() => {
+            step += 1;
+            const shouldCheckBudget = (step & 1023) === 0;// Binary bitmask check: Only execute the inner code once every 1024 steps since perf.now is heavy
+            if (shouldCheckBudget) checkBudget();
+        },
+        afterEachCall:(result)=>{
+            if (!(result instanceof Error)) {//if the result is an error,we let the interpreter bubble it up
+                checkBudget();//in case the function doesnt use up to the number of steps required to recheck the budget,we check the budget here to be accurate and safe
+            }
+        }
+    });
+
+    console.log(`Finished building the fn in ${(performance.now()-fnBuilsStart).toFixed(3)}ms`);
+    return monitoredFn
+}
+
+
+const avg = timeFn(calculateAverage,50);
+
+perf(()=>{
+    const result = avg([20,30,70,88,91,72],'monitor');
+    console.log('\nThe average from the timed fn is: ',result);
+});
