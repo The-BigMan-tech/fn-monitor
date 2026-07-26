eee2e07e1da5836ef605fdde65a91c847339be0b
The-BigMan-tech <saddlebox9@gmail.com>
2026-07-26 19:26:55 +0100

    refactor: rename a function

--- src/lifecycle-functions.ts
+++ src/lifecycle-functions.ts
@@ -30,7 +30,7 @@ export function callPerExe(interpreter:SvalPlus) {
 //we want to reset the variables each time before we call the monitor so that each child evaluation dont get leaked refs or values from their parents.but we exclude eval stack and exe stack because they must be tracked throughout all evaluations
 export function callInspector(acornNode:AcornNode,currentScope:Scope<SvalPlus>,handler:Reusables['handler']) {
     const interpreter = currentScope.interpreter!;
-    refreshReusables(acornNode,currentScope,handler)
+    updateReusables(acornNode,currentScope,handler)
     return interpreter.inspector!(interpreter.visit);//by the time the call monitor is called,this is guaranteed to not be null
 }
 
@@ -79,7 +79,7 @@ export function pushResult(interpreter:SvalPlus,result:any) {
 
 
 
-function refreshReusables(acornNode:AcornNode,currentScope:Scope<SvalPlus>,handler:Reusables['handler']) {
+function updateReusables(acornNode:AcornNode,currentScope:Scope<SvalPlus>,handler:Reusables['handler']) {
     const interpreter = currentScope.interpreter!;
     interpreter.reusables.node = acornNode as EsNode;
     interpreter.reusables.currentScope = currentScope;
