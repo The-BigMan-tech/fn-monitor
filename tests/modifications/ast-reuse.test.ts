a490383d3dab41b235ee7785320e573f6d1f49b2
The-BigMan-tech <saddlebox9@gmail.com>
2026-07-28 14:35:44 +0100

    test: finish an async test under the ast reuse behaviour

--- tests/modifications/ast-reuse.test.ts
+++ tests/modifications/ast-reuse.test.ts
@@ -37,25 +37,22 @@ describe('AST Mutation Persistence', () => {
         expect(monitoredFn(4, 3)).toBe(1);//The function executes as `4 - 3`, resulting in 1 (proving persistence).
     });
 
-    it('[Async] should parse the function into an AST once and reuse it, causing mutations to persist across calls', () => {
+    it('[Async] should parse the function into an AST once and reuse it, causing mutations to persist across calls',async () => {
         let modifiedOp = false;
 
-        function add(a: number, b: number) {
-            return a + b;
+        async function add(a: number, b: number) {
+            return await Promise.resolve(a + b);
         }
         const monitoredFn = monitor({//this is intentionally not lifted to the top because its doing a stateful operation and it should not affect other tests
             main: { 
-                ref: add 
+                ref:add
             },
             inspector:(visit) => {
                 // Only mutate on the first execution to prove persistence
                 if (modifiedOp) return;
-                visit.is('ReturnStatement', (event) => {
-                    const arg = event.node.argument;
-                    if (arg?.type === "BinaryExpression") {
-                        arg.operator = "-"; // Mutate the AST node from '+' to '-'
-                        modifiedOp = true;
-                    }
+                visit.is('BinaryExpression', (event) => {
+                    event.node.operator = "-"; // Mutate the AST node from '+' to '-'
+                    modifiedOp = true;
                 });
             }
         });
@@ -63,12 +60,12 @@ describe('AST Mutation Persistence', () => {
         expect(modifiedOp).toBe(false);
 
         // First call: The inspector mutates the AST. 
-        expect(monitoredFn(5, 1)).toBe(4);// The function executes as `5 - 1`, resulting in 4.
+        expect(await monitoredFn(5, 1)).toBe(4);// The function executes as `5 - 1`, resulting in 4.
 
 
         // Second call: The inspector skips mutation, BUT the AST is already changed.
         expect(modifiedOp).toBe(true);//verify that the operator is modified first.
-        expect(monitoredFn(4, 3)).toBe(1);//The function executes as `4 - 3`, resulting in 1 (proving persistence).
+        expect(await monitoredFn(4, 3)).toBe(1);//The function executes as `4 - 3`, resulting in 1 (proving persistence).
     });
 
     it('[Sync] should reuse ast nodes across different monitored functions only if they have the exact same configuration.',()=>{
