1520cfcf314b5a52ff0aa3ab0b4e2f92b250cae2
The-BigMan-tech <saddlebox9@gmail.com>
2026-07-30 17:37:12 +0100

    test: verify another behaviour of inspectors

--- tests/modifications/others.test.ts
+++ tests/modifications/others.test.ts
@@ -12,6 +12,33 @@ describe('Other Runtime Behaviours',()=>{
                 }
             }
         });
-        expect(()=>fn()).toThrow(ReferenceError)
+        expect(()=>fn()).toThrow(ReferenceError);
     });
+
+    it('[Sync] should ensure that an inspector always watches the monitored function on every call.',()=>{
+        let count = 0;
+        let calledInspector = false;
+
+        const fn = monitor({
+            main:{
+                ref:()=>undefined
+            },
+            beforeEachCall:()=>{
+                calledInspector = false;
+            },
+
+            //if the interpreter doesnt properly reset to the monitoring state on every call, 
+            // this hook wont fire and it will reflect in the count and fail the test
+            inspector:()=>{
+                if (!calledInspector) {//this ensures that it only mutates the count once per call
+                    count += 1;
+                    calledInspector = true
+                }
+            }
+        })
+        for (let i = 1;i < 10;i++) {
+            fn();
+            expect(count).toBe(i);
+        }
+    })
 })
