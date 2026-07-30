a304f62f285545b7bebb8fe838c8e5f254d16b31
The-BigMan-tech <saddlebox9@gmail.com>
2026-07-30 09:23:39 +0100

    test: move some tests from the visit tests to where they belong

--- tests/modifications/fundamentals.test.ts
+++ tests/modifications/fundamentals.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { InspectorGenerator, LangEvent,monitor } from '../../src/index'; 
+import { Visit } from '../../src/sval-plus';
 
 describe('Fundamental Runtime Behaviour',()=>{
     
@@ -218,5 +219,82 @@ describe('Fundamental Runtime Behaviour',()=>{
 
         const result5 = monitoredAdd5(2,2).next().value
         expect(result5).toBe(4);
+    })
+
+    it('[Sync] should ensure that all the hooks are fired when set and together with their proper arguments',()=>{
+        let calledBeforeCallHook = false;
+        let calledAfterCallHook = false;
+        let calledInspectorHook = false;
+        let calledOnStepHook = false;
+
+        const fn = monitor({
+            main:{
+                ref:(x:number)=>x**2
+            },
+            beforeEachCall:(x)=>{
+                calledBeforeCallHook = true;
+                expect(x).toBe(10)
+            },
+            afterEachCall:(result)=>{
+                calledAfterCallHook = true;
+                expect(result).toBe(100)
+            },
+            inspector:(visit)=>{
+                calledInspectorHook = true;
+                expect(visit instanceof Visit).toBe(true)
+            },
+            onStep:((args:any)=>{
+                calledOnStepHook = true;
+                expect(args).toBe(undefined)
+            }) as any
+        })
+        fn(10);
+        expect(calledBeforeCallHook).toBe(true);
+        expect(calledAfterCallHook).toBe(true);
+        expect(calledInspectorHook).toBe(true);
+        expect(calledOnStepHook).toBe(true)
+    })
+
+    it('[Sync] should ensure that the beforeEachCall and afterEachCall hooks are only fired once per function call',()=>{
+        let beforeHookCount = 0;
+        let afterHookCount = 0;
+
+        const fn = monitor({
+            main:{
+                ref:()=>undefined
+            },
+            beforeEachCall:()=>{
+                beforeHookCount += 1;
+            },
+            afterEachCall:()=>{
+                afterHookCount += 1;
+            }
+        })
+        fn();
+        expect(beforeHookCount).toBe(1);
+        expect(afterHookCount).toBe(1)
+    })
+
+    it('[Async] should ensure that the beforeEachCall and afterEachCall hooks are only fired once per function call',async ()=>{ 
+        //We test the afterEachCall hook again for the async version because the interpreter handles this particular hook differently for async functions 
+        let afterHookCount = 0;
+
+        //im only adding this count for consistency
+        let beforeHookCount = 0;
+
+        const fn2 = monitor({
+            main:{
+                ref:async ()=>undefined
+            },
+            beforeEachCall:()=>{
+                beforeHookCount += 1;
+            },
+            afterEachCall:()=>{
+                afterHookCount += 1;
+            }
+        })
+        await fn2();
+        expect(beforeHookCount).toBe(1);
+        expect(afterHookCount).toBe(1)
     })
 })
