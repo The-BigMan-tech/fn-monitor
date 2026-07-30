965cac9c291c9b5f4f39635191c24e94f2a631ea
The-BigMan-tech <saddlebox9@gmail.com>
2026-07-30 16:47:02 +0100

    fix: ensure that monitored funcs run in strict mode
    - The codebase passed the test

--- src/index.ts
+++ src/index.ts
@@ -166,7 +166,8 @@ export function monitor<T extends Fn>(setup:MonitorFnSetup<T>):T & {alreadyMonit
     
     const fnSrc = interpreter.getFnSrc(mainFn,capturesLabel);
     fnSrc.fnCode += interpreter.getFnSources(functionsToEmbed);
-    
+
+    fnSrc.fnCode = `'use strict'\n${fnSrc.fnCode}`;//ensure that it runs in strict mode
     interpreter.useFn(fnSrc);
 
     if (sourceOut) {//only write the generated code if the interpreter could parse it
