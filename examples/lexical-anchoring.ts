6e9c638f8cd7a93c365fcf93a04acf4e7d692331
The-BigMan-tech <saddlebox9@gmail.com>
2026-08-26 12:27:40 +0100

    docs(lexical-anchoring): heavily simplify the example while retaining correctness

--- examples/lexical-anchoring.ts
+++ examples/lexical-anchoring.ts
@@ -1,42 +1,14 @@
-import { 
-    monitor,
-    LangEvent,
-    VarDeclEvent
-} from "../src/index.ts";
+import { monitor, LangEvent } from "../src/index.ts";
 
-const anchor = {
-    write:(varName:string,event:VarDeclEvent) => {
-        const node = event.node;
-        const scope = event.scope;
-        const lexicalAnchor = scope.variables.search(varName);
+let lexicalAnchor:number | null = null;
 
-        if (typeof lexicalAnchor !== "number") {
-            node.declarations.forEach(decl=>{
-                const isLexicalDepth = (decl.id.type === 'Identifier') && (decl.id.name === varName);
-                if (isLexicalDepth) {
-                    if (decl.init?.type === "Literal") {
-                        decl.init.value = scope.depth;
-                        decl.init.raw = scope.depth.toString();
-                    }
-                }
-            })
-        }
-    },
-    getRelativeDepth:(varName:string,event:LangEvent):number | null => {
-        const scope = event.scope;
-        const lexicalAnchor = scope.variables.search(varName) as number | null;
-        if (typeof lexicalAnchor !== "number") {
-            return null
-        }
-        return scope.depth - lexicalAnchor
-    }
+function relativeDepth(anchor:number,event:LangEvent):number {
+    return event.scope.depth - anchor
 }
-
 const fn = monitor({
     main: {
         ref: () => {
             const helper = ()=>{
-                let lexicalAnchor = null;// artifically insert our anchor
                 if (true) {
                     let x = 10;
                     x++;
@@ -50,17 +22,18 @@ const fn = monitor({
         const currentFn = visit.callStack().get(0)
 
         if (currentFn.name === "helper") { // enter the helper
-            const lexicalAnchorLabel = 'lexicalAnchor';
-
-            visit.is('VariableDeclaration',event=>{
-                anchor.write(lexicalAnchorLabel,event);
-            })
+            if (!lexicalAnchor) {
+                visit.is('Any', event => {
+                    lexicalAnchor = event.scope.depth;
+                });
+                return;
+            }
             visit.is('UpdateExpression', event=>{
-                const depth = anchor.getRelativeDepth(lexicalAnchorLabel,event)
+                const depth = relativeDepth(lexicalAnchor!,event)
                 console.log('Lexical depth of helper\'s update expression: ',depth);
             })
             visit.is('ReturnStatement', event=>{
-                const depth = anchor.getRelativeDepth(lexicalAnchorLabel,event)
+                const depth = relativeDepth(lexicalAnchor!,event)
                 console.log('Lexical depth of helper\'s return statement: ',depth);
             })
         }
